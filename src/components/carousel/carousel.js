6f4ead13f6b87992e63c07ec903b8e7d5b9204cf
Sydney Krantz <syd107@gmail.com>
2020-08-05 18:56:56 -0700

    added both carousels and section title

--- src/components/carousel/carousel.js
+++ src/components/carousel/carousel.js
@@ -1,47 +1,27 @@
-import React, { Component } from 'react';
-import Carousel from 'react-bootstrap/Carousel';
- 
- 
-export default function Carousel1() {
+import React, { Component } from "react";
+import "./carousel.css"
+
+import Slider from "react-slick";
+
+export default function Carousel1(props) {
+  var settings = {
+    dots: true,
+    infinite: true,
+    fade: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    className: "slides",
+  };
+
   return (
-  
-      <Carousel>
-  <Carousel.Item>
-    <img
-      className="d-block w-100"
-      src="holder.js/800x400?text=First slide&bg=373940"
-      alt="First slide"
-    />
-    <Carousel.Caption>
-      <h3>First slide label</h3>
-      <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-    </Carousel.Caption>
-  </Carousel.Item>
-  <Carousel.Item>
-    <img
-      className="d-block w-100"
-      src="holder.js/800x400?text=Second slide&bg=282c34"
-      alt="Third slide"
-    />
-
-    <Carousel.Caption>
-      <h3>Second slide label</h3>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-    </Carousel.Caption>
-  </Carousel.Item>
-  <Carousel.Item>
-    <img
-      className="d-block w-100"
-      src="holder.js/800x400?text=Third slide&bg=20232a"
-      alt="Third slide"
-    />
-
-    <Carousel.Caption>
-      <h3>Third slide label</h3>
-      <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-    </Carousel.Caption>
-  </Carousel.Item>
-</Carousel>
-  )   
+    <div className="carousel">
+      <Slider {...settings}>
+        <div>{props.message.section_title.toUpperCase()}</div>
+        <div quote={props.message.quotes.message}>
+          {console.log(props.message.quotes[0].message)}
+        </div>
+      </Slider>
+    </div>
+  );
 }
-  
